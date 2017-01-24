e601ff90cb63541a7cecd0303bace8380986e1f2
laigf <452354033@qq.com>
2017-01-24 10:10:18 +0800

    Initial Commit

---
+++ NewRDBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewRDBeacon
+//
+//  Created by totyu3 on 17/1/24.
+//  Copyright © 2017年 NIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
